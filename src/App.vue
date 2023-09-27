4c36caccd2c452850fa6abefb2f9f2eb65179a12
MoY8462 <moy.sav21@hotmail.com>
2023-09-27 15:56:52 -0600

    build: :construction: component planet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Navbar from './components/Navbar/Navbar.vue';
+import Planet from './components/Planet/Planet.vue'
 </script>
 
 <template>
   <Navbar/>
+  <Planet/>
 </template>
---
+++ src/components/Planet/Planet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grid grid-cols-3 gap-6 px-12">
+        <section class="col-span-2 flex justify-center">
+            <img class="h-[600px]" src="@/assets/planets/earth.svg" alt="Planeta">
+        </section>
+        <section>
+            <h1>EARTH</h1>
+            <p>La Tierra es tu hogar, un planeta único en el vasto universo. Con una diversidad asombrosa, te ofrece montañas majestuosas, vastos océanos, selvas exuberantes y desiertos misteriosos. Su atmósfera te brinda el aire que respiras y protege contra el frío del espacio.</p>
+            <label for="">
+                <span>Source: </span>
+                <a href="#">Wikipedia</a>
+                <img src="@/assets/icons/arrow-top-right-on-square.svg" alt="">
+            </label>
+        </section>
+    </div>
+</template>
